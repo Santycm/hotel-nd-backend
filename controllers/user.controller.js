734cd774f29bac25cd7643f14eaf024913b7311c
Santycm <santicmoreno@gmail.com>
2024-09-04 14:20:14 -0500

    add login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,10 +4,18 @@ import jwt from "jsonwebtoken";
 
 const register = async (req, res) => {
   try {
-    const {name, lastname, cedula, tel, email, username, password } = req.body;
+    const { name, lastname, cedula, tel, email, username, password } = req.body;
     console.log(req.body);
 
-    if (!name || !lastname || !cedula || !tel || !email || !username || !password) {
+    if (
+      !name ||
+      !lastname ||
+      !cedula ||
+      !tel ||
+      !email ||
+      !username ||
+      !password
+    ) {
       return res
         .status(400)
         .json({ ok: false, message: "Missing required fields" });
@@ -37,7 +45,7 @@ const register = async (req, res) => {
       expiresIn: "1h",
     });
 
-    return res.status(201).json({ ok: true, token:token });
+    return res.status(201).json({ ok: true, token: token });
   } catch (err) {
     console.log(err);
     return res
@@ -48,6 +56,33 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "Missing required fields" });
+    }
+
+    const user = await UserModel.findOneByEmail(email);
+
+    if (!user) {
+      return res.status(400).json({ ok: false, message: "User not found" });
+    }
+
+    const isMatch = bcryptjs.compareSync(password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    return res.status(200).json({ ok: true, token: token });
   } catch (err) {
     console.log(err);
     return res
